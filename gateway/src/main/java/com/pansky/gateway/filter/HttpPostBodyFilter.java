4c065d7d0d096382948dbb677339df80da3cc8c5
foWQL <283109037@qq.com>
2023-02-27 19:53:45 +0800

    网关服务：获取body体全局过滤器

---
+++ gateway/src/main/java/com/pansky/gateway/filter/HttpPostBodyFilter.java
@@ -0,0 +1,66 @@
+package com.pansky.gateway.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.UnsupportedEncodingException;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class HttpPostBodyFilter implements GlobalFilter, Ordered {
+    public static final String CACHE_REQUEST_BODY_OBJECT_KEY = "POST_BODY";
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String method = request.getMethodValue();
+        String contentType = request.getHeaders().getFirst("Content-Type");
+        if ("POST".equals(method)){
+            return DataBufferUtils.join(exchange.getRequest().getBody())
+                    .flatMap(dataBuffer -> {
+                        byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                        dataBuffer.read(bytes);
+                        try {
+                            String bodyString = new String(bytes, "utf-8");
+                            log.info("bodyString {}",bodyString);
+                            //将body请求数据缓存到 Attributes 中
+                            exchange.getAttributes().put(CACHE_REQUEST_BODY_OBJECT_KEY,bodyString);
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                        DataBufferUtils.release(dataBuffer);
+                        Flux<DataBuffer> cachedFlux = Flux.defer(() -> {
+                            DataBuffer buffer = exchange.getResponse().bufferFactory()
+                                    .wrap(bytes);
+                            return Mono.just(buffer);
+                        });
+
+                        ServerHttpRequest mutatedRequest = new ServerHttpRequestDecorator(exchange.getRequest()) {
+                            @Override
+                            public Flux<DataBuffer> getBody() {
+                                return cachedFlux;
+                            }
+                        };
+                        //封装request，传给下一级    
+                        return chain.filter(exchange.mutate().request(mutatedRequest).build());
+                    });
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -200;
+    }
+}
